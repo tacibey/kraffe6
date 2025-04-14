6cf234de830065c46088b5f6052c7f394897cb56
Taci <taciyalcin@gmail.com>
2025-04-14 15:00:41 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -92,7 +92,7 @@ export default defineConfig({
       SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
       Head: "./src/components/ui/starlight/Head.astro",
       MobileMenuFooter: "./src/components/ui/starlight/MobileMenuFooter.astro",
-      ThemeSelect: "./src/components/ui/starlight/ThemeSelect.astro",
+      
     },
     head: [
       {
