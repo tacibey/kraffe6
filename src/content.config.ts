04736d49fc2c4c745ddcf953dc0eb0ee602ed471
Taci <taciyalcin@gmail.com>
2025-05-09 14:23:29 +0300

    Update content.config.ts [skip ci]

--- src/content.config.ts
+++ src/content.config.ts
@@ -69,7 +69,6 @@ const blogCollection = defineCollection({
   schema: ({ image }) => z.object ({
   title: z.string(),
   description: z.string(),
-  contents: z.array(z.string()),
   author: z.string(),
   role: z.string().optional(),
   authorImage: image(),
